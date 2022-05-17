04ca1415707014c5a125573db5072ea43284e6df
Luca Severo <luca98.severo@gmail.com>
2022-05-17 17:56:56 +0200

    Create feature done!

--- 05-02-models-and-crud/evening-course/app/controllers/pets_controller.rb
+++ 05-02-models-and-crud/evening-course/app/controllers/pets_controller.rb
@@ -6,4 +6,21 @@ class PetsController < ApplicationController
   def show
     @pet = Pet.find(params[:id])
   end
+
+  def new
+    @pet = Pet.new
+  end
+
+  def create
+    pet = Pet.new(pets_params)
+    pet.save
+
+    redirect_to pet_path(pet)
+  end
+
+  private
+
+  def pets_params
+    params.require(:pet).permit([:name, :species, :color, :address, :date])
+  end
 end
--- 05-02-models-and-crud/evening-course/config/routes.rb
+++ 05-02-models-and-crud/evening-course/config/routes.rb
@@ -2,5 +2,10 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # verb '/path', to: 'controller#action'
   get '/pets', to: 'pets#index'
+
+  get '/pets/new', to: 'pets#new'
+  post '/pets', to: 'pets#create'
+
   get '/pets/:id', to: 'pets#show', as: 'pet'
+
 end
