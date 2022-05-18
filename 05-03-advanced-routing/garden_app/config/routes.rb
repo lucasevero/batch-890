707396197784bfa99293848c50aee590ecd6c557
Luca Severo <luca98.severo@gmail.com>
2022-05-18 17:28:06 +0200

    Gardens done

--- 05-03-advanced-routing/garden_app/config/routes.rb
+++ 05-03-advanced-routing/garden_app/config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
+  resources :gardens
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
+
+# See all gardens
+# See the details of a garden
+# See all plants
+# Add a new plant
+# Delete a plant
--- 05-03-advanced-routing/garden_app/db/seeds.rb
+++ 05-03-advanced-routing/garden_app/db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+if Rails.env.development?
+  puts 'Cleaning the database'
+  Garden.destroy_all
+end
+
+puts 'Lets create some gardens'
+Garden.create({
+  name: "My beautiful garden",
+  banner_url: 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_1.jpg'
+  })
+  puts 'Garden 1 created'
+  Garden.create({
+    name: "Another beautiful garden",
+    banner_url: 'https://raw.githubusercontent.com/lewagon/fullstack-images/master/rails/parks-and-plants/garden_2.jpg'
+    })
+  puts 'Garden 2 created'
