d41ede01e3159304cbaf5ab57fa07573853eefec
Luca Severo <luca98.severo@gmail.com>
2022-05-25 12:05:15 +0200

    Added pundit lecture

--- 05-07-authorization-and-pundit/lecture/app/controllers/restaurants_controller.rb
+++ 05-07-authorization-and-pundit/lecture/app/controllers/restaurants_controller.rb
@@ -1,9 +1,10 @@
+
 class RestaurantsController < ApplicationController
   before_action :set_restaurant, only: [:show, :edit, :update, :destroy]
 
   # GET /restaurants
   def index
-    @restaurants = Restaurant.all
+    @restaurants = policy_scope(Restaurant)
   end
 
   # GET /restaurants/1
@@ -13,6 +14,7 @@ class RestaurantsController < ApplicationController
   # GET /restaurants/new
   def new
     @restaurant = Restaurant.new
+    authorize @restaurant
   end
 
   # GET /restaurants/1/edit
@@ -22,6 +24,8 @@ class RestaurantsController < ApplicationController
   # POST /restaurants
   def create
     @restaurant = Restaurant.new(restaurant_params)
+    @restaurant.user = current_user
+    authorize @restaurant
 
     if @restaurant.save
       redirect_to @restaurant, notice: 'Restaurant was successfully created.'
@@ -49,10 +53,11 @@ class RestaurantsController < ApplicationController
     # Use callbacks to share common setup or constraints between actions.
     def set_restaurant
       @restaurant = Restaurant.find(params[:id])
+      authorize @restaurant
     end
 
     # Only allow a list of trusted parameters through.
     def restaurant_params
-      params.require(:restaurant).permit(:name, :user_id)
+      params.require(:restaurant).permit(:name)
     end
 end
--- 05-07-authorization-and-pundit/lecture/db/seeds.rb
+++ 05-07-authorization-and-pundit/lecture/db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+me = User.create(email: "luca@me.com", password: "123456", admin: true)
+not_me = User.create(email: "boyan@notme.com", password: "123456")
+
+Restaurant.create({
+  name: "Nish Nash",
+  user: me
+})
+Restaurant.create({
+  name: "Wooza Bowls",
+  user: not_me
+})
+Restaurant.create({
+  name: "Meson El Cordobes",
+  user: me
+})
+Restaurant.create({
+  name: "Restaurante Maracanã Rodizio",
+  user: me
+})
+Restaurant.create({
+  name: "El Inca",
+  user: not_me
+})
+Restaurant.create({
+  name: "Al Andalus",
+  user: not_me
+})
+Restaurant.create({
+  name: "Coyacan",
+  user: me
+})
