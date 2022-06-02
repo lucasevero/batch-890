03bb1a42af94575fec21204875ad10afb574a00c
Luca Severo <luca98.severo@gmail.com>
2022-06-02 11:59:49 +0200

    ajax class added

---
+++ 05-10-ajax-in-rails/lecture/app/javascript/controllers/insert_in_list_controller.js
@@ -0,0 +1,20 @@
+import { Controller } from "@hotwired/stimulus";
+import { csrfToken } from "@rails/ujs";
+
+export default class extends Controller {
+  static targets = ["items", "form"];
+
+  send(event) {
+    event.preventDefault();
+
+    fetch(this.formTarget.action, {
+      method: 'POST',
+      headers: { 'Accept': 'application/json', 'X-CSRF-Token': csrfToken() },
+      body: new FormData(this.formTarget)
+    })
+      .then(response => response.json())
+      .then((data) => {
+        console.log(data)
+      })
+  }
+}
---
+++ 05-10-ajax-in-rails/lecture/app/views/reviews/create.json.jbuilder
@@ -0,0 +1,7 @@
+# if @review.persisted?
+#   json.form json.partial!("reviews/form.html.erb", restaurant: @restaurant, review: Review.new)
+#   json.inserted_item json.partial!("restaurants/review.html.erb", review: @review)
+# else
+#   json.form json.partial!("reviews/form.html.erb", restaurant: @restaurant, review: @review)
+# end
+json.hey "hey"
